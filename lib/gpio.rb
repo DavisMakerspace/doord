c88c979073f9eb5f2ac25b1323156c95c3626f7b
Braden <contact@dabrado.net>
2013-08-04 16:56:56 -0700

    Add gpio poller class; use it in sentryd

--- lib/gpio.rb
+++ lib/gpio.rb
@@ -107,3 +107,22 @@ class GPIO
   end
 end
 
+class GPIOPoller
+  def initialize(gpios = [], timeout = nil)
+    @gpios = gpios
+    @timeout = timeout
+  end
+  def run()
+    while @gpios.size > 0
+      io2gpio = {}
+      gpios.each { |g| io2gpio[g.instance_variable_get(:@value_file)] = g }
+      ready = IO.select(nil, nil, io2gpio.keys, @timeout)
+      if ready
+        ready[2].each { |io| yield io2gpio[io] }
+      else
+        yield nil
+      end
+    end
+  end
+  attr_accessor :gpios, :timeout
+end
