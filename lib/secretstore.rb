431d1c1eab497c1f1561ea7b5d94840080396a5d
Braden <contact@dabrado.net>
2013-08-31 17:08:34 -0700

    Replaced TokenRegistry with SecretStore

---
+++ lib/secretstore.rb
@@ -0,0 +1,66 @@
+require 'yaml/store'
+require 'openssl'
+require 'base64'
+
+class SecretStore
+  READ_ONLY = true
+  def initialize(path)
+    @store = YAML::Store.new path
+  end
+  def create(id)
+    @store.transaction do
+      return false if @store.root?(id)
+      @store[id] = {secret: nil, data: nil}
+    end
+    return true
+  end
+  def reset(id)
+    @store.transaction() do
+      return false if !@store.root?(id)
+      @store[id][:secret] = nil
+    end
+    return true
+  end
+  def auth?(id, secret)
+    secret_digest = digest secret
+    @store.transaction(READ_ONLY) do
+      return false if !@store.root?(id)
+      @store.abort if @store[id][:secret] == nil
+      return @store[id][:secret] == secret_digest
+    end
+    @store.transaction() do
+      return false if !@store.root?(id)
+      @store[id][:secret] = secret_digest
+    end
+    return true
+  end
+  def exists?(id)
+    @store.transaction(READ_ONLY) do
+      return @store.root?(id)
+    end
+  end
+  def delete(id)
+    @store.transaction do
+      return false if !@store.root?(id)
+      @store.delete id
+    end
+    return true
+  end
+  def attach(id, data)
+    @store.transaction do
+      return false if !@store.root?(id)
+      @store[id][:data] = data
+    end
+    return true
+  end
+  def view(id)
+    data = nil
+    @store.transaction(READ_ONLY) do
+      data = @store[id][:data] if @store.root?(id)
+    end
+    return data
+  end
+  def digest(string)
+    return Base64.strict_encode64 OpenSSL::Digest::SHA512.digest string
+  end
+end
