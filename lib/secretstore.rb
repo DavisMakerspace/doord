4f0a0cb53ec22b2a537e3fd1dbf838a0ae50b596
Braden <contact@dabrado.net>
2013-09-11 21:54:39 -0700

    Use salted key for secret store; don't store extra data; no implicit secret setting

--- lib/secretstore.rb
+++ lib/secretstore.rb
@@ -1,43 +1,20 @@
 require 'yaml/store'
 require 'openssl'
-require 'base64'
 
 class SecretStore
-  READ_ONLY = true
+  CRYPT_ITER = 20000
+  DIGEST = OpenSSL::Digest::SHA512
+  SALT_BYTES = 16
+  DISABLING_PREFIX = '!'
   def initialize(path)
     @store = YAML::Store.new path, true  # Be thread safe; though perhaps more cleanly handled if done by methods that use it...
   end
-  def add(id, data=nil)
+  def add(id)
     @store.transaction do
       return false if @store.root?(id)
-      @store[id] = {secret: nil, data: data}
+      @store[id] = {key: '', salt: ''}
     end
     return true
-  end
-  def reset(id)
-    @store.transaction() do
-      return false if !@store.root?(id)
-      @store[id][:secret] = nil
-    end
-    return true
-  end
-  def auth?(id, secret)
-    secret_digest = digest secret
-    @store.transaction(READ_ONLY) do
-      return false if !@store.root?(id)
-      @store.abort if @store[id][:secret] == nil
-      return @store[id][:secret] == secret_digest
-    end
-    @store.transaction() do
-      return false if !@store.root?(id)
-      @store[id][:secret] = secret_digest
-    end
-    return true
-  end
-  def exists?(id)
-    @store.transaction(READ_ONLY) do
-      return @store.root?(id)
-    end
   end
   def remove(id)
     @store.transaction do
@@ -46,21 +23,50 @@ class SecretStore
     end
     return true
   end
-  def attach(id, data)
-    @store.transaction do
+  def set(id, secret)
+    salt = OpenSSL::Random.random_bytes(SALT_BYTES)
+    @store.transaction() do
       return false if !@store.root?(id)
-      @store[id][:data] = data
+      @store[id][:key] = make_key(secret, salt)
+      @store[id][:salt] = salt
     end
     return true
   end
-  def view(id)
-    data = nil
-    @store.transaction(READ_ONLY) do
-      data = @store[id][:data] if @store.root?(id)
+  def disable(id)
+    @store.transaction() do
+      return false if !@store.root?(id)
+      return true if @store[id][:key].start_with?(DISABLING_PREFIX)
+      @store[id][:key].prepend DISABLING_PREFIX
     end
-    return data
+    return true
   end
-  def digest(string)
-    return Base64.strict_encode64 OpenSSL::Digest::SHA512.digest string
+  def disabled?(id)
+    @store.transaction() do
+      return @store.root?(id) ? @store[id][:key].start_with?(DISABLING_PREFIX) : false
+    end
+  end
+  def undisable(id)
+    @store.transaction() do
+      return false if !@store.root?(id)
+      return true if !@store[id][:key].start_with?(DISABLING_PREFIX)
+      @store[id][:key][0] = ''
+    end
+    return true
+  end
+  def auth?(id, secret)
+    entry = nil
+    @store.transaction(true) do
+      entry = @store[id] if @store.root?(id)
+    end
+    return entry ? (entry[:key] == make_key(secret, entry[:salt])) : false
+  end
+  def exists?(id)
+    @store.transaction(true) do
+      return @store.root?(id)
+    end
+  end
+  def make_key(secret, salt)
+    digest = DIGEST.new
+    return OpenSSL::PKCS5.pbkdf2_hmac(secret, salt, CRYPT_ITER, digest.digest_length, digest)
   end
 end
